8cdb96665d4e723a1814356fac99e22fa1a2f3c8
CLCK0622 <zhongyi070622@163.com>
2024-11-11 16:42:54 +0800

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   return (
     <>
       <Head>
-        <title>2512 Album</title>
+        <title>Next.js Conf 2022 Photos</title>
         <meta
           property="og:image"
           content="https://nextjsconf-pics.vercel.app/og-image.png"
@@ -51,12 +51,19 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         )}
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
           <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+            <div className="absolute inset-0 flex items-center justify-center opacity-20">
+              <span className="flex max-h-full max-w-full items-center justify-center">
+                <Bridge />
+              </span>
+              <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
+            </div>
             <Logo />
             <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
-              2512 Photo Gallery
+              2022 Event Photos
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Welcome to HSEFZ 2512!
+              Our incredible Next.js community got together in San Francisco for
+              our first ever in-person conference!
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
@@ -64,7 +71,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               target="_blank"
               rel="noreferrer"
             >
-              Homepage(W.I.P)
+              Clone and Deploy
             </a>
           </div>
           {images.map(({ id, public_id, format, blurDataUrl }) => (
@@ -79,7 +86,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               <Image
                 alt="Next.js Conf photo"
                 className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-                style={{ transform: 'translate3d(0, 0, 0)' }}
+                style={{ transform: "translate3d(0, 0, 0)" }}
                 placeholder="blur"
                 blurDataURL={blurDataUrl}
                 src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
@@ -95,29 +102,34 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         </div>
       </main>
       <footer className="p-6 text-center text-white/80 sm:p-12">
-        Site is modified by{" "}
+        Thank you to{" "}
         <a
-          href="https://github.com/CLCK0622"
+          href="https://edelsonphotography.com/"
           target="_blank"
           className="font-semibold hover:text-white"
           rel="noreferrer"
         >
-          Kevin Zhong
+          Josh Edelson
         </a>
-        .{" Photos are mainly shot by "}
+        ,{" "}
         <a
-          href=""
+          href="https://www.newrevmedia.com/"
           target="_blank"
           className="font-semibold hover:text-white"
           rel="noreferrer"
         >
-          2512cr
+          Jenny Morgan
         </a>
-        .
-        <br />
-        Thanks to 2512 for all the pictures and happy memories.
-        <br />
-        All made with ❤. Copyright © 2024-
+        , and{" "}
+        <a
+          href="https://www.garysextonphotography.com/"
+          target="_blank"
+          className="font-semibold hover:text-white"
+          rel="noreferrer"
+        >
+          Gary Sexton
+        </a>{" "}
+        for the pictures.
       </footer>
     </>
   );
